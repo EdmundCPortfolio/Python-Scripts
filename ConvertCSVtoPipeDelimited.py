1185808c56d5427e5d26826f3e9861edb0c3cc25
EdmundCPortfolio <149627639+EdmundCPortfolio@users.noreply.github.com>
2024-03-01 22:35:03 +0000

    Update ConvertCSVtoPipeDelimited.py

--- ConvertCSVtoPipeDelimited.py
+++ ConvertCSVtoPipeDelimited.py
@@ -21,21 +21,21 @@ def process_csv(input_folder: str, output_folder: str):
             os.makedirs(output_folder)
             print(f"Created output folder: {output_folder}")
 
-        # Construct input and output paths
+        # Input and output paths
         input_csv_path = os.path.join(input_folder, 'population.csv')
         output_txt_path = os.path.join(output_folder, 'population.txt')
 
-        # Read the CSV file into a DataFrame
+        # Read in orginal CSV file
         df = pd.read_csv(input_csv_path, sep=',')
 
-        # Write the DataFrame to the output file with pipe delimiter
+        # Output text file with pipe delimiter
         df.to_csv(output_txt_path, sep='|', index=False)
 
-        # Create the 'processed' folder if it doesn't exist
+        # Create the Processed folder if it doesn't exist
         processed_folder = os.path.join(output_folder, 'processed')
         os.makedirs(processed_folder, exist_ok=True)
 
-        # Move the original CSV file to the 'processed' folder
+        # Move the original CSV file to the Processed folder
         original_csv_filename = os.path.basename(input_csv_path)
         new_csv_path = os.path.join(processed_folder, original_csv_filename)
         os.rename(input_csv_path, new_csv_path)
@@ -48,7 +48,7 @@ def process_csv(input_folder: str, output_folder: str):
     except PermissionError as e:
         print(f"Permission error: {e}")
 
-# Example usage:
+
 def main():
     input_folder_path = r'D:\NationalData\Load\ImportData\Input'
     output_folder_path = r'D:\NationalData\Load\Output'
